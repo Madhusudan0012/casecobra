ec94d5b82e533a918651071b82da179d6041f854
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-12-23 02:04:34 +0530

    create user reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 import { Check, Star } from 'lucide-react'; 
 import Phone from '@/components/phone'
+import { Icons } from "@/components/Icons";
 export default function Home(){
   return (
     <div className='bg-slate-50 grainy-light'>
@@ -107,7 +110,93 @@ export default function Home(){
           </MaxWidthWrapper>
 
           {/**Value proposition section */}
-        </section>       
-      </div>
+
+        </section>   
+        <section className='bg-slate-100 grainy-dark py-24'>
+        <MaxWidthWrapper className='flex flex-col items-center gap-16 sm:gap-32'>
+          <div className='flex flex-col lg:flex-row items-center gap-4 sm:gap-6'>
+            <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+              What our{' '}
+              <span className='relative px-2'>
+                customers{' '}
+                <Icons.underline className='hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500' />
+              </span>{' '}
+              say
+            </h2>
+            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+          </div> 
+          <div className = 'mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8  xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the design . Had the case for two and a half months now and <span className="p-0.5 bg-slate-800 text-white"> the image is super clear</span>, on the case I had before , the image started fading into yellow-ish color after a couple weeks. Love you Madhusudan"
+                </p>
+
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img 
+                 className="rounded-full h-12 w-12 object-cover"
+                 src = '/users/user-1.png'
+                 alt = 'user'
+                 />
+                 <div className="flex flex-col">
+                  <p className="font-semibold"> Ram Kumar Sharma </p>
+                  <div className="flex gap-1.5 items-center text-zinc-600 ">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <p className="text-sm">
+                      Verified Purchase
+                    </p>
+                    
+                     </div>
+
+
+                 </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8  xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+                <Star className="h-5 w-5 text-green-600 fill-green-600"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I Usually keep my phone together with my eys in my pocket and that led to some pretty heavy scratchmarks on all of my last phone cases<span className="p-0.5 bg-slate-800 text-white"> looks brand new after about half a year</span> god to see cobra at top.
+                </p>
+
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img 
+                 className="rounded-full h-12 w-12 object-cover"
+                 src = '/users/user-2.png'
+                 alt = 'user'
+                 />
+                 <div className="flex flex-col">
+                  <p className="font-semibold"> Modi Ji </p>
+                  <div className="flex gap-1.5 items-center text-zinc-600 ">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <p className="text-sm">
+                      Verified Purchase
+                    </p>
+                    
+                     </div>
+
+
+                 </div>
+              </div>
+            </div>
+          </div>
+          </MaxWidthWrapper>
+          </section>
+          </div>
   );
 }
