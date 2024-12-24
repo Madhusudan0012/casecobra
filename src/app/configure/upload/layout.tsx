e1128fff5dc5cc937e72a249bed0fea55137f7db
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-12-25 02:04:13 +0530

    commit

--- src/app/configure/upload/layout.tsx
+++ src/app/configure/upload/layout.tsx
@@ -1,4 +1,11 @@
-const Layout =() =>{
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import {ReactNode} from 'react' 
+const Layout =({children} : {children: ReactNode}) =>{
+    return (
+        <MaxWidthWrapper className="flex-1 flex flex-col">
+            {children}
+        </MaxWidthWrapper>
+    )
 
 }
 
