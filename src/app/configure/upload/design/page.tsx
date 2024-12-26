1b5e3a7a88e167604aeea823cb2604478cb59384
Madhusudan_chauhan <madhusudanchauhan62@gmail.com>
2024-12-27 02:52:36 +0530

    commit

--- src/app/configure/upload/design/page.tsx
+++ src/app/configure/upload/design/page.tsx
@@ -1,4 +1,20 @@
-const Page  = async() =>{
-    return <p></p>
+
+interface PageProps {
+    searchParams:{
+        [key : string] : string[] | undefined
+
+    }
+}
+
+
+
+
+
+const Page  = async({searchParams} : PageProps) =>{
+
+    const {id} = searchParams
+    //make the db call
+
+    return <p>{id}</p>
 }
 export default Page 
